95e0651d62168839b8466676c3a73e14a2faa919
tashfeenrao <raotashfeen25@gmail.com>
2020-09-15 02:29:32 +0500

    form submitted with state values

--- src/components/presentational/SignUp.js
+++ src/components/presentational/SignUp.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SignUp = () => (
-  <div>
-    <h1>Sign up page</h1>
-  </div>
-);
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const signUp = e => {
+    e.preventDefault();
+    setUsername('');
+    setPassword('');
+  };
+  return (
+    <div>
+      <form onSubmit={signUp}>
+        <label htmlFor="username">
+          Username
+          <input
+            type="text"
+            name="username"
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="username"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label htmlFor="password">
+          Password_Confirmation
+          <input
+            type="password"
+            name="username"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <input type="submit" value="SignUp" />
+      </form>
+    </div>
+  );
+};
 
 export default SignUp;
