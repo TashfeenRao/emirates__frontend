212398d4ac7161825ebec9a6cc68e9e87d8b9bce
tashfeenrao <raotashfeen25@gmail.com>
2020-09-19 03:41:38 +0500

    firebase backeend setup

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDiH9w-b7dzSSHvD7BEhGfIQTXxNv988-c',
+  authDomain: 'emirates-backend.firebaseapp.com',
+  databaseURL: 'https://emirates-backend.firebaseio.com',
+  projectId: 'emirates-backend',
+  storageBucket: 'emirates-backend.appspot.com',
+  messagingSenderId: '1091226720462',
+  appId: '1:1091226720462:web:4b8c1d716e91cae07d189f',
+};
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
